4706286684d83ff80319052e353557f762bfdb10
raythompsonwebdev <ray_thomp@hushmail.com>
2022-01-30 20:58:00 +0000

    added cypress -add blog route adding blog

--- server.js
+++ server.js
@@ -5,48 +5,56 @@ const posts = require("./posts.js");
 const server = express();
 
 // serve static files
-const staticHandler = express.static('public');;
+const staticHandler = express.static("public");
 server.use(staticHandler);
 
 server.get("/", (request, response) => {
+  //assign post items to empty string
   let postitems = "";
-  for (const post of posts) {
+
+  // loop over array of post object key and value entries ;
+  for (const post of Object.entries(posts)) {
+    // deconstruct array in post constant
+    const [user, postItem] = post;
+
+    console.log(postItem);
+    // add postItem values to post
     postitems += `<article class="blog-post">
                 <header class="blog-header">
-                  <h1 class="blog-title">${post.title}</h1>
-                  <h2>User : ${post.user}</h2>
+                  <h1 class="blog-title">${postItem.title}</h1>
+                  <h2>User : ${user}</h2>
                 </header>
                 
-                <p>${post.blogpost}</p>
-                <footer class="blog-footer">Time submitted: <time>${post.date}</time></footer>      
+                <p>${postItem.blogpost}</p>
+                <footer class="blog-footer">Time submitted: <time>${postItem.date}</time></footer>      
               </article>`;
   }
   const html = `
   <!doctype html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Say Something Nice!</title>
-      <link href="css/style.css" type="text/css" rel="stylesheet"> 
-    </head>
-    <body>
-    <nav>
-      <ul>
-      <li>
-          <a href="/">Home</a>
-        </li>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Say Something Nice!</title>
+        <link href="css/style.css" type="text/css" rel="stylesheet"> 
+      </head>
+      <body>
+      <nav>
+        <ul>
         <li>
-          <a href="/add-post">add post</a>
-        </li>
-        
-      </ul>
-    </nav>
-    <main>
-      <h1>Say Something Nice!</h1>
-        ${postitems}
-      </main>
-    </body>
-  </html>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/add-post">add post</a>
+          </li>
+          
+        </ul>
+      </nav>
+      <main>
+        <h1>Say Something Nice!</h1>
+          ${postitems}
+        </main>
+      </body>
+    </html>
   `;
   response.send(html);
 });
@@ -59,7 +67,7 @@ server.get("/add-post", (request, response) => {
   <html>
     <head>
       <meta charset="utf-8">
-      <title>POst!</title>
+      <title>Post!</title>
     </head>
     <body>
     <nav>
@@ -76,11 +84,11 @@ server.get("/add-post", (request, response) => {
     <main>
       <h1>Create a Post</h1>
       <form method="POST">
-        <label id="username">DUser name</label>
-        <input id="username" name="username">
+        <label id="name">User name</label>
+        <input id="name" name="name">
 
-        <label id="blogtitle">Blog Title</label>
-        <input id="blogtitle" name="blogtitle">
+        <label id="title">Blog Title</label>
+        <input id="title" name="title">
 
         <label id="mood">Mood</label>
         <input id="mood" name="moode">
@@ -88,9 +96,9 @@ server.get("/add-post", (request, response) => {
         <label id="blogpost">Blog Post</label>
         <textarea id="blogpost" name="blogpost"></textarea>
 
-        <label id="meeting-time">Time</label>
-        <input type="datetime-local" id="meeting-time"
-       name="meeting-time" value="2018-06-12T19:30"
+        <label id="date">Time</label>
+        <input type="datetime-local" id="date"
+       name="date" value="2018-06-12T19:30"
        min="2018-06-07T00:00" max="2018-06-14T00:00">
 
         <button>Search</button>
@@ -104,12 +112,10 @@ server.get("/add-post", (request, response) => {
 
 server.post("/add-post", bodyParser, (request, response) => {
   const newPost = request.body;
-  const username = newPost.username.toLowerCase();
-  const blogpost = newPost.blogpost;
-
-  console.log(username, blogpost);
-  //posts[username] = newPost;
-  //response.redirect("/");
+  const name = newPost.name.toLowerCase();
+  console.log(newPost);
+  posts[name] = newPost;
+  response.redirect("/");
 });
 
 const PORT = 3333;
